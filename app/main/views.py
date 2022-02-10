43b278683459c6a9ec8a0f6276edde9da8fb7676
theccode <captainaddae@msn.com>
2022-02-10 10:25:21 +0000

    about, learn, research, upload pages added

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,19 @@ from . import main
 @main.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@main.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@main.route('/upload', methods=['GET', 'POST'])
+def upload():
+    return render_template('upload_form.html')
+
+@main.route('/learn', methods=['GET'])
+def learn():
+    return render_template('learn.html')
+
+@main.route('/research', methods=['GET'])
+def research():
+    return render_template('research.html')
