801208cb0086ccc6394fe7128fa9045b7d7c742d
Your Name <akonobeajosiefyn@gmail.com>
2022-02-11 19:20:24 +0000

    added drop-downs for About page,seach tab,and overview page

--- app/main/views.py
+++ app/main/views.py
@@ -5,18 +5,21 @@ from . import main
 def index():
     return render_template('index.html')
 
-@main.route('/about')
+@main.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
 
-@main.route('/upload')
+@main.route('/upload', methods=['GET', 'POST'])
 def upload():
     return render_template('upload_form.html')
 
-@main.route('/learn')
+@main.route('/learn', methods=['GET'])
 def learn():
     return render_template('learn.html')
 
-@main.route('/research')
+@main.route('/research', methods=['GET'])
 def research():
     return render_template('research.html')
+@main.route('/overview',methods=['GET'])
+def overview():
+    return render_template('overview.html')    
